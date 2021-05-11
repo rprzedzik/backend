a925d6f0eadecd9b6defc936a8cfd8b708def694
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-05-11 13:35:20 +0200

    add submodule

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Copyright © Ergonode Sp. z o.o. All rights reserved.
- * See LICENSE.txt for license details.
- */
-
-declare(strict_types=1);
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
